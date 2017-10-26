bf1a60b2e282ec1d64da115e84616d44b759dc1f
GuessWh0o <developerint97@gmail.com>
2017-10-26 21:58:17 +0300

    Delete all files except the biggest one

---
+++ AndroidFileUtils/DeleteAllFilesExceptTheBiggestFromDir.java
@@ -0,0 +1,47 @@
+package com.activaire.video.player.util;
+
+import android.util.Log;
+
+import java.io.File;
+
+/**
+ * Created by Maks on 10/26/2017.
+ */
+
+public class DeleteAllFilesExceptTheBiggestFromDir {
+
+    public static void deleteAllSmallFiles() {
+        File yourDir = new File("pathToYourFolder");
+        boolean exists = (yourDir.mkdir() || yourDir.isDirectory());
+        if (exists) {
+            boolean moreThan1 = false;
+            File[] files = yourDir.listFiles();
+            if (files.length > 1) {
+                moreThan1 = true;
+                File theBiggestFile = null;
+                float theBiggestFileSize = 0.1f;
+                for (final File fileEntry : yourDir.listFiles()) {
+                    float newFileLength = fileEntry.length();
+                    if (newFileLength > theBiggestFileSize) {
+                        theBiggestFile = fileEntry;
+                        theBiggestFileSize = newFileLength;
+                    }
+                }
+                if (theBiggestFile != null && moreThan1) {
+                    for (final File fileEntry : yourDir.listFiles()) {
+                        if (fileEntry.getName().equals(theBiggestFile.getName())) {
+                            continue;
+                        } else {
+                            if (fileEntry.delete()) {
+                                Log.d("DELETED", ": " + fileEntry.getName());
+                            } else {
+                                Log.d("NOT DELETED", ": " + fileEntry.getName());
+                            }
+                        }
+                    }
+                    Log.d("BIGGEST FILE", ": " + theBiggestFile.getName());
+                }
+            }
+        }
+    }
+}
